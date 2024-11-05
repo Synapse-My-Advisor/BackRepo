a92355a818a72efb2ff351b44e7b49c05afead36
Mateus Robers <robersmateus@gmail.com>
2024-11-05 11:10:16 -0300

    fix requisition

--- backend/src/vendor/llm.js
+++ backend/src/vendor/llm.js
@@ -5,7 +5,7 @@ function prompt() {
         const options = {
             method: 'post',
             url: 'http://127.0.0.1:5000/home',
-            body: '{"message":"teste"}',
+            body: '{"content":"resuma o conteudo","tg_id":1, "user_id":"mateus"}',
             headers:{
                 "Content-Type":"application/json"
             }
